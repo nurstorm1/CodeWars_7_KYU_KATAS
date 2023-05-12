34cc6461b777a4dee64de51da87eb200562c2f4e
Nuridin Aiupdzhanov <114620322+nurstorm1@users.noreply.github.com>
2023-05-12 03:08:57 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+const secretNumber = '7';
+if (Number(secretNumber) === 7) {
+    console.log('Ugadal strogo');
+}
+
+if (secretNumber == 7) {
+    console.log('Ugadal ne strogo');
+
+}
+
+const q = prompt('Vvedite chislo');
+if (q == 7) {
+    console.log('Ugadal ne strogo');
+}
